f1e8acd3a2c001ef2676c28763ef84c809fe89cb
pexApred <120075705+pexApred@users.noreply.github.com>
2024-12-19 10:39:48 -0500

    Created Logout and Login Routes

--- app/routes/api.py
+++ app/routes/api.py
@@ -21,15 +21,42 @@ def signup():
         db.add(newUser)
         db.commit()
     except:
-        print(sys.exe_info()[0])
+        print(sys.exc_info()[0])
 
         # insert failed, so rollback and send error to front end
         db.rollback()
         return jsonify(message = 'Signup failed'), 500
 
-    # you can create sessions in Flask only if you've defined a secret key. Fortunately, the following code in app/__init__.py does just that: app.config.from_mapping(SECRET_KEY='super_secret_key'). In a production environment, you should change this key to something that's harder to guess.
-
+@bp.route('/users/logout', methods=['POST'])
+def logout():
+    # remove session variables
     session.clear()
-    session['user_id'] = newUser.id
+    return '', 204
+
+@bp.route('/users/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    db = get_db()
+
+    try:
+        # query user by email
+        user = db.query(User).filter(User.email == data['email']).one()
+
+        # check if password is correct
+        if user.verify_password(data['password']) == False:
+            return jsonify(message = 'Incorrect credentials'), 400
+        
+        session.clear()
+        session['user_id'] = user.id
+        session['loggedIn'] = True
+        return jsonify(id = user.id)
+    
+    except:
+        print(sys.exc_info()[0])
+        
+    # you can create sessions in Flask only if you've defined a secret key. Fortunately, the following code in app/__init__.py does just that: app.config.from_mapping(SECRET_KEY='super_secret_key'). In a production environment, you should change this key to something that's harder to guess.
+    session.clear()
+    session['user_id'] = user.id
     session['loggedIn'] = True
-    return jsonify(id = newUser.id)
+    
+    return jsonify(id = user.id)
